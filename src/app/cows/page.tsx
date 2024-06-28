01bfb5b938038a77200f346829c950ecfb09bb26
Sam Sweeney <samsweeneyy@gmail.com>
2024-06-28 17:54:45 +1000

    Commit

--- src/app/cows/page.tsx
+++ src/app/cows/page.tsx
@@ -4,7 +4,6 @@
 export default function Cows() {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <iframe width="100%" height="900" frameborder="0" scrolling="no" src="https://tafensw-my.sharepoint.com/personal/sam_sweeney7_studytafensw_edu_au1/_layouts/15/Doc.aspx?sourcedoc={986f2316-9fd4-4f1b-a00c-2f6e021ed7f2}&action=embedview&wdAllowInteractivity=False&wdInConfigurator=True&wdInConfigurator=True"></iframe>
     </div>
   )
 }
